cdb3d82150358374763d1fd9db5c9c3d9735c0c1
fatimaabubakaradam <zahrammcy@gmail.com>
2025-01-04 17:35:53 -0800

    update server

--- server.js
+++ server.js
@@ -1,22 +1,22 @@
-  
-  const express = require('express');
-  const mongoose = require('mongoose');
-  const app = express();
-  app.use(express.json());
+const express = require('express');
+const mongoose = require('mongoose');
+require('dotenv').config(); 
 
-  const MONGO_URI = 'mongodb+srv://fatimammcy:Faag10661@flowerappcluster.tcjxo.mongodb.net/?retryWrites=true&w=majority&appName=FlowerAppCluster';
+const app = express();
+app.use(express.json());
 
- mongoose
-    .connect(MONGO_URI)
-    .then(() => {
-      const PORT = 5001;
-      app.listen(PORT, () => {
+const MONGO_URI = process.env.MONGODB_URI; 
+const PORT = process.env.PORT_NUMBER || 5001; 
+
+mongoose
+  .connect(MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => {
+    app.listen(PORT, () => {
       console.log(`Server is running on port ${PORT}`);
     });
   })
-
-  .catch(( error) => {
-    console.error(  error);
+  .catch((error) => {
+    console.error('Error connecting to MongoDB:', error);
   });
 
 app.get('/', (req, res) => {
